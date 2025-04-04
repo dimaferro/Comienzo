da4329880cc627f4a021e246dbcc9ddbee404f2c
dimaferro <difernan27@gmail.com>
2025-04-03 23:14:57 -0300

    Add files via upload

---
+++ Area predefinida.py
@@ -0,0 +1,9 @@
+def area(l,a):
+        return l * a
+print("El área de un rectángulo se calcula multiplicando la base por la altura")
+l=20
+a=30
+print("el valor del largo es:",l)
+print("el valor del ancho es:",a)
+resultado = area(l,a)
+print("El área del rectángulo es:",resultado)
---
+++ Suma 2 enteros.py
@@ -0,0 +1,7 @@
+def suma(a,b):
+    
+    return a + b
+resultado = suma(8,9)
+print("el resultado es",resultado)
+resultado = suma(10,12)
+print("El resultado ahora es",resultado)
